210633d5014ccd10aab5bd411db317a30f20dd3c
Syuji <commentonly.is@gmail.com>
2020-02-16 00:22:33 +0900

    自由文読み上げ機能 /freetext を追加した

--- osc_yukkuri.rb
+++ osc_yukkuri.rb
@@ -2,6 +2,7 @@ require 'osc-ruby'
 require 'osc-ruby/em_server'
 require 'dotenv'
 require 'date'
+require 'shellwords'
 
 Dotenv.load
 
@@ -13,7 +14,12 @@ server = OSC::EMServer.new(oscport)
 
 server.add_method '/jihou' do
     timestr = Time.now.strftime("%Y年%m月%d日%H時%M分")
-    `#{aquestalk_path} "現在は、#{timestr}です。" | aplay`
+    system(aquestalk_path, '"現在は、#{timestr}です。" | aplay')
+end
+
+server.add_methid '/freetext' do |message|
+    arg = "'#{Shellwords.escape(message)}'' | aplay"
+    system(aquestalk_path, arg)
 end
 
 server.run

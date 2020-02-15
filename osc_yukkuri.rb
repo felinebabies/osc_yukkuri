112a5e3278200da442f876ad263fcf2f1ac613f0
Syuji <commentonly.is@gmail.com>
2020-02-16 00:51:26 +0900

    時報の文字列生成時、0パディングをしないように修正した。

--- osc_yukkuri.rb
+++ osc_yukkuri.rb
@@ -13,7 +13,7 @@ aquestalk_path = ENV['AQUESTALK_PATH'] || '/home/pi/tool/aquestalkpi/AquesTalkPi
 server = OSC::EMServer.new(oscport)
 
 server.add_method '/jihou' do
-    timestr = Time.now.strftime("%Y年%m月%d日%H時%M分")
+    timestr = Time.now.strftime("%Y年%-m月%-d日%-H時%-M分")
     `#{aquestalk_path} "現在は、#{timestr}です。" | aplay`
 end
 

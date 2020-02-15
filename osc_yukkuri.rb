90aba7efc7dcf6ffe479c30dc5c65b528abf8458
Syuji <commentonly.is@gmail.com>
2020-02-15 22:58:05 +0900

    最初のコミット 時報機能実装

---
+++ osc_yukkuri.rb
@@ -0,0 +1,19 @@
+require 'osc-ruby'
+require 'osc-ruby/em_server'
+require 'dotenv'
+require 'date'
+
+Dotenv.load
+
+oscport = ENV['PORT'] || 3333
+
+aquestalk_path = ENV['AQUESTALK_PATH'] || '/home/pi/tool/aquestalkpi/AquesTalkPi'
+
+server = OSC::EMServer.new(oscport)
+
+server.add_method '/jihou' do
+    timestr = Date.today.to_time.strftime("%Y年%m月%d日 %H時%M分")
+    `#{aquestalk_path} "現在は、#{timestr}です。" | aplay`
+end
+
+server.run
